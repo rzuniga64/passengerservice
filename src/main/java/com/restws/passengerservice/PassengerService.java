ef748931b7522593da4dd356ab90bc25792b24a4
Raul Zuniga <rzuniga64@gmail.com>
2018-09-30 18:53:49 -0500

    Initial commit

---
+++ src/main/java/com/restws/passengerservice/PassengerService.java
@@ -0,0 +1,45 @@
+package com.restws.passengerservice;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+
+import com.restws.model.Passenger;
+
+@Produces("application/xml,application/json")
+@Consumes("application/xml,application/json,application/x-www-form-urlencoded")
+@Path("/passengerservice")
+public interface PassengerService {
+
+	/**
+	 *  Get the list of passengers
+	 * 	@param start the start of the list of passengers.
+	 * 	@param size the size of the list of passengers.
+	 * 	@return the list of passengers.
+	 */
+	@Path("/passengers")
+	@GET
+    List<Passenger> getPassengers();
+	//List<Passenger> getPassengers(@QueryParam("start") int start, @QueryParam("size") int size);
+
+	/**
+	 * 	Add a passenger to the list of passengers.
+	 * 	@param firstName the first name of the passenger.
+	 * 	@param lastName	the last name of the passenger.
+	 * 	@param agent	the agent that added the passenger.
+	 * 	@param headers	the HTTP headers.
+	 */
+	@Path("/passengers")
+	@POST
+    Passenger addPassenger(Passenger passenger);
+	//void addPassenger(@FormParam("firstName") String firstName, @FormParam("lastName") String lastName, @HeaderParam("agent") String agent, @Context HttpHeaders headers);
+}
---
+++ src/main/java/com/restws/passengerservice/PassengerserviceApplication.java
@@ -0,0 +1,12 @@
+package com.restws.passengerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassengerserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassengerserviceApplication.class, args);
+    }
+}
